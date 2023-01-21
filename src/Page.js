b38c349b3a0474e65599245842cd4da2c2538215
Mesih Kilinc <mesihkilinc@gmail.com>
2023-01-21 16:03:39 -0600

    center the contents

--- src/Page.js
+++ src/Page.js
@@ -3,26 +3,28 @@ import ReactSequenceViewer from 'react-sequence-viewer';
 import BasicDataTable from './BasicDataTable';
 import MUIDataTable from './MUIDataTable';
 import PDB from './PDB';
+import { Grid } from '@mui/material';
+
 
   const Page = ({ data }) => {
     return (
-      <div>
+      <Grid container align="center" justify="center" direction="column">
         {Object.keys(data).map((key, index) => {
           const value = data[key];
           console.log(key,value)
           if (key.startsWith('p:')) //it's a page don't render it.
             return
           else if (key === 'Seq')
-            return <ReactSequenceViewer sequence={value}  key={index} />
+            return <ReactSequenceViewer sequence={value}  key={2*index} />
           else if (key === 'Table'){
-            return <MUIDataTable {...value} key={index}/>
+            return <Grid item key={2*index}><MUIDataTable {...value} key={2*index+1}/></Grid>
           }
           else if (key === 'PDB')
-            return <PDB {...value} key={index}/>
+            return <Grid item key={2*index}><PDB {...value} key={2*index+1}/></Grid>
           else
-            return React.createElement(key, { key: index }, value)
+            return React.createElement(key, { key: 2*index }, value)
         })}
-      </div>
+      </Grid>
     );
   };
 
