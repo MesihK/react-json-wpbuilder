b2f3b0a8fc999a54818c262042475bba972f421d
Mesih Kilinc <mesihkilinc@gmail.com>
2023-01-21 20:48:09 -0600

    wrap PDB with a card element

--- src/PDB.js
+++ src/PDB.js
@@ -1,5 +1,7 @@
 import {React, useMemo} from 'react';
 import { Stage, Component } from 'react-ngl';
+import { Card, CardContent, Typography, Box } from '@mui/material';
+
 
 
 function PDB({path, width='600px', height='400px'}) {
@@ -9,9 +11,15 @@ function PDB({path, width='600px', height='400px'}) {
     const params={ backgroundColor: "white" };
     console.log(path,reprList)
     return (
-        <Stage width={width} height={height} params={params}>
-            <Component path={path} reprList={reprList} />
-        </Stage>
+        <Box sx={{ height: height, width: width }}>
+            <Card>
+            <CardContent>
+                <Stage width={width} height={height} params={params}>
+                <Component path={path} reprList={reprList} />
+                </Stage>  
+            </CardContent>
+            </Card>
+        </Box>
     );
 }
 export default PDB;
