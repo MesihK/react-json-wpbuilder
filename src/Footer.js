17d646142b6094d9e0db768704c773f9b30ec315
Mesih Kilinc <mesihkilinc@gmail.com>
2023-01-25 13:24:39 -0600

    add site icon to footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,15 +2,21 @@ import * as React from 'react';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import ScreenShareIcon from '@mui/icons-material/ScreenShare';
 
 export default function Footer() {
   return (
     <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
         <Grid container direction="column" alignItems="center">
           <Grid item xs={12}>
-            <Typography color="black" variant="subtitle1">
-              JSON Web Page Renderer
-            </Typography>
+            <div style={{
+                display: 'flex',
+                alignItems: 'center',
+                flexWrap: 'wrap',
+            }}>
+              <ScreenShareIcon sx={{mr: 1}} />
+              <Typography color="black" variant="subtitle1">JSON Web Page Renderer</Typography>
+            </div>  
           </Grid>
           <Grid item xs={12}>
             <Typography color="textSecondary" variant="subtitle2">
