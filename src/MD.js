30354319345cf7f43b719a286f6f445f7db8f0f7
Mesih Kilinc <mesihkilinc@gmail.com>
2023-01-24 22:24:23 -0600

    update website files

--- src/MD.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactMarkdown from 'react-markdown'
-import remarkGfm from 'remark-gfm'
-
-function MD({markdown}) {
-  return <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} />
-}
-export default MD;
