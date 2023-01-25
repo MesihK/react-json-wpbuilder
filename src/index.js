e4e90725d1b77aa475af3c073a53e9335b0c0c69
Mesih Kilinc <mesihkilinc@gmail.com>
2023-01-24 22:39:07 -0600

    Github pages serves this website on username.github.io/repoName so fix the links

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'
 import { BrowserRouter as Router } from "react-router-dom";
-
+const repoName = 'react-json-wpbuilder'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
+  <Router basename={`/${repoName}`}>
     <App />
   </Router>
 );
