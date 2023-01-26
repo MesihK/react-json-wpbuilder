229722613a617c094d85e526877ffc199742c372
Mesih Kilinc <mesihkilinc@gmail.com>
2023-01-25 20:08:37 -0600

    use mui-markdown for markdown rendering

--- src/MD.js
+++ src/MD.js
@@ -1,8 +1,17 @@
 import React from 'react'
-import ReactMarkdown from 'react-markdown'
+/*import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
+//import rehypeHighlight from 'rehype-highlight'
+//rehypePlugins={[rehypeHighlight]}
 
 function MD({markdown}) {
-  return <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} />
+  return <ReactMarkdown  remarkPlugins={[remarkGfm]}>
+    {markdown}
+  </ReactMarkdown>
+}*/
+
+import MuiMarkdown from 'mui-markdown';
+function MD({markdown}) { 
+  return <MuiMarkdown>{markdown}</MuiMarkdown>
 }
 export default MD;
