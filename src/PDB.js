ce3780c2d4fb448418682fa7a9e5a46926e52eea
Mesih Kilinc <mesihkilinc@gmail.com>
2023-02-03 17:29:29 -0600

    [PDB] use one variable for pdb and fetch the link before NGL.js

--- src/PDB.js
+++ src/PDB.js
@@ -4,6 +4,7 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
 import { Typography } from '@mui/material';
+import { red } from '@mui/material/colors';
 
 const debounce = (func, delay) => {
     let debounceHandler;
@@ -15,9 +16,9 @@ const debounce = (func, delay) => {
     };
 };
 
-function PDB({ path, name, pdb="",
-               repr={ type: 'cartoon', params: { colorScheme: "sstruc", smoothSheet: true } },
-               height = '400px' }) {
+function PDB({ pdb, name,
+    repr = { type: 'cartoon', params: { colorScheme: "sstruc", smoothSheet: true } },
+    height = '400px' }) {
 
     const reprList = useMemo(() => [
         repr
@@ -25,34 +26,51 @@ function PDB({ path, name, pdb="",
     const params = { backgroundColor: "white" };
 
     let [cameraState, setCameraState] = useState();
+    let [textPDB, setTextPDB] = useState([pdb,""]);
+    let [err, setErr] = useState("");
 
     const handleCameraMove = debounce(nextCameraState => {
         setCameraState(nextCameraState);
     }, 300);
 
-    let loadFileParams = { 
+    let loadFileParams = {
         name: '',
         status: '',
         visible: true,
+        ext: "pdb"
     };
 
-    if (pdb !== ""){
-        path = new Blob( [ pdb ], { type: 'text/plain'} );
-        loadFileParams.ext = "pdb"
+    if (pdb.startsWith("rcsb")) {
+        pdb = pdb.split("://")[1]
+        pdb = "https://files.rcsb.org/download/" + pdb + ".pdb";
+    }
+    
+    if (pdb.startsWith("http")) {
+        if (textPDB[1] === "" || textPDB[0] !== pdb)
+            fetch(pdb).then(response => {
+                if (!response.ok) throw new Error("Fetch error!", { cause: response });
+                else return response.text()
+            }).then(response => {
+                setTextPDB([pdb,response]);
+                setErr("");
+            }).catch(error => setErr(error.message));
+    } else {
+        textPDB[1] = pdb;
     }
 
-    console.log(loadFileParams,path);
+    let pdbFile = new Blob([textPDB[1]], { type: 'text/plain' });
 
+    if (err !== "") return <Typography backgroundColor={red[300]}>PDB Fetch Error! message: {err}, link: {pdb}</Typography>
     return (
         <Paper elevation={1}>
             <Box textAlign='center'>
                 {name && <Typography variant='subtitle1'>{name}</Typography>}
                 <Stage height={height} params={params} cameraState={cameraState} onCameraMove={handleCameraMove} >
-                    <Component path={path} reprList={reprList} loadFileParams={loadFileParams} onLoad={() => {
+                    <Component path={pdbFile} reprList={reprList} loadFileParams={loadFileParams} onLoad={() => {
                         if (cameraState == null) setTimeout(() => setCameraState({}), 200);
                     }} />
                 </Stage>
-                <Button variant="outlined" sx={{mb:1}} onClick={() => {
+                <Button variant="outlined" sx={{ mb: 1 }} onClick={() => {
                     setCameraState({});
                 }}>Reset Camera</Button>
             </Box>
