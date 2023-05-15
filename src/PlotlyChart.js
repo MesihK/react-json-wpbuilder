6c4b6063ed657e0aa1a2c19cac48ac4ff0f00cb9
Mesih Kilinc <mesihkilinc@gmail.com>
2023-05-15 07:53:35 -0500

    Fix plot margins, add ability to show titles

--- src/PlotlyChart.js
+++ src/PlotlyChart.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import Plotly from "plotly.js-basic-dist-min";
 import createPlotlyComponent from "react-plotly.js/factory";
+import { TitleSharp } from '@mui/icons-material';
 
 const Plot = createPlotlyComponent(Plotly);
 
-export default function PlotlyChart({data, height}) {
+export default function PlotlyChart({data, height, xtitle="", ytitle="", title=""}) {
     return <Plot data={data}
     useResizeHandler
     style={{ width: '100%', height: '100%' }}
     layout={{
       autosize: true,
-      height: height
+      height: height,
+      "margin":{"l":40,"r":0,"t":30,"b":30},
+      "title": { "text": title },
+      "xaxis": { "title": xtitle },
+      "yaxis": { "title": ytitle}
     }}/>
 }
